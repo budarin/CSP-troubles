15f730be19a0f4b7419976a2115fda150b0178a7
Vadim Budarin <budarin-vv@yandex.ru>
2021-05-19 11:52:58 +0300

    fix: ode

--- folder/non-root-script.js
+++ folder/non-root-script.js
@@ -1,5 +1,5 @@
-const el = document.getElementById("non-root-external-script");
-el.innerText = "Ok";
-el.style.color = "green";
+const el2 = document.getElementById("non-root-external-script");
+el2.innerText = "Ok";
+el2.style.color = "green";
 
 console.log("non root script is allowed");
--- root-script.js
+++ root-script.js
@@ -1,5 +1,5 @@
-const el = document.getElementById("root-external-script");
-el.innerText = "Ok";
-el.style.color = "green";
+const el1 = document.getElementById("root-external-script");
+el1.innerText = "Ok";
+el1.style.color = "green";
 
 console.log("root script is allowed");
