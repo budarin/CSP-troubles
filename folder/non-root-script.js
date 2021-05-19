640c84f14418ba4403ef1824e201e58d96ff8484
Vadim Budarin <budarin-vv@yandex.ru>
2021-05-19 12:48:36 +0300

    fix: code

--- folder/non-root-script.js
+++ folder/non-root-script.js
@@ -1,4 +1,4 @@
 const el2 = document.getElementById("non-root-external-script");
 el2.innerText = "Ok";
-
+el2.style.color = "green";
 console.log("non root script is allowed");
--- root-script.js
+++ root-script.js
@@ -1,4 +1,4 @@
 const el1 = document.getElementById("root-external-script");
 el1.innerText = "Ok";
-
+el1.style.color = "green";
 console.log("root script is allowed");
